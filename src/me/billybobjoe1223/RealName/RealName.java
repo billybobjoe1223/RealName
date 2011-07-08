cd0fc68194c707251963d860bc9e621aec60f026
Tommy <billybobjoe1223@wrathmc.com>
2011-07-08 08:11:34 -0700

    Added permissions support

--- src/me/billybobjoe1223/RealName/RealName.java
+++ src/me/billybobjoe1223/RealName/RealName.java
@@ -7,22 +7,29 @@ import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+
 public class RealName extends JavaPlugin {
+	public static PermissionHandler permissionHandler;
 	Logger log = Logger.getLogger("Minecraft");
 	public void onEnable() {
-		log.info("RealName has been enabled");
+		setupPermissions();
+		log.info("[RealName] RealName has been enabled");
 	}
 	public void onDisable() {
-		log.info("RealName has been disabled");
+		log.info("[RealName] RealName has been disabled");
 	}
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		Server server = sender.getServer();
 		Player[] players = server.getOnlinePlayers();
-		if(cmd.getLabel().equalsIgnoreCase("rname")) {
-			if(args.length == 1) {
+		Player player = (Player) sender;
+ 		if(cmd.getLabel().equalsIgnoreCase("rname") && args.length == 1) {
+			if(permissionHandler.has(player, "realname.rname")) {
 				int i;
 				for(i = players.length - 1; i >= 0; i--) {
 					if(args[0].equalsIgnoreCase(players[i].getDisplayName())) {
@@ -34,9 +41,23 @@ public class RealName extends JavaPlugin {
 						}
 					}
 				} 
-				return true;
+			} else {
+				sender.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
 			}
+			return true;
 		}
-		return false;
+ 		return false;
+	}
+	private void setupPermissions() {
+	    
+	    Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
+	    
+	    if (permissionsPlugin == null) {
+	        log.info("Permission system not detected, defaulting to OP");
+	        return;
+	    }
+	    
+	    permissionHandler = ((Permissions) permissionsPlugin).getHandler();
+	    log.info("[RealName] Found and will use plugin "+((Permissions)permissionsPlugin).getDescription().getFullName());
 	}
 }
